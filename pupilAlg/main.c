4b2cae0f6b065ed2e9599447d93a7cce087733ac
tianying107 <starfrombeijing@gmail.com>
2016-10-03 14:36:21 -0700

    Initial Commit

---
+++ pupilAlg/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pupilAlg
+//
+//  Created by st chen on 16/10/3.
+//  Copyright © 2016年 Star Chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
